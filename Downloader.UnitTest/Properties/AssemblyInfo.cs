f5e18f1e441fdff2aa4aea938b26647cc4ec4260
Ditschke <kevin.ditschke@vescon.com>
2020-01-17 08:20:18 +0100

    View and ViewModel added, UnitTest project created

---
+++ Downloader.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Downloader.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Downloader.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ade394d0-88ea-4a0e-9808-6d1f461f2adc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Downloader/MainViewModel.cs
+++ Downloader/MainViewModel.cs
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 
 namespace Downloader
 {
-    class DownloadList
+    class MainViewModel
     {
-
-
-
     }
 }
