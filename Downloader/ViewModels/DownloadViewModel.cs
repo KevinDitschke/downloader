c75f2fa1f0b11b5619450be1025e70ab0a41d75a
Ditschke <kevin.ditschke@vescon.com>
2020-02-13 14:43:58 +0100

    Added Property DownloadStarted to activate and deactivate start and stop buttons

--- Downloader/ViewModels/DownloadViewModel.cs
+++ Downloader/ViewModels/DownloadViewModel.cs
@@ -12,30 +12,37 @@ namespace Downloader
         public double Progress { get; set; }
         public string Name { get; set; }
         public string URL { get; set; }
+        public bool DownloadStarted { get; set; } = false;
 
         public DownloadViewModel(IDownloader downloader, IMessenger messenger)
         {
             _downloader = downloader;
             _messenger = messenger;
         }
-        public bool CanStartDownload => !(_downloader == null);
-        public bool CanStopDownload => !(_downloader == null);
+        public bool CanStartDownload => !DownloadStarted;
+        public bool CanStopDownload => DownloadStarted;
         public async void StartDownload()
         {
             var progress = new Progress<double>(value => { Progress = value; });
-
+            DownloadStarted = true;
             try
             {
                 var success = await _downloader.Start(URL, Name, progress);
 
                 if (success)
+                {
+                    DownloadStarted = false;
                     _messenger.DisplayMessage("The file was successfully downloaded!", "Success!");
+                }
                 else if (!success)
+                {
+                    DownloadStarted = false;
                     _messenger.DisplayMessage("Download has been stopped!", "Error!");
+                }
             }
             catch (HttpRequestException)
             {
-
+                DownloadStarted = false;
                 _messenger.DisplayMessage("Verbindungsfehler", "Error!");
 
             }
@@ -44,6 +51,7 @@ namespace Downloader
         public void StopDownload()
         {
             _downloader.Stop();
+            DownloadStarted = false;
         }
     }
 }
