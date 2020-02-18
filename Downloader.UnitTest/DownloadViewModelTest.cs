3bb1214a6dd67f134aad4320fe603a90b51003e8
Kevin Ditschke <kevin.ditschke@vescon.com>
2020-02-18 15:29:19 +0100

    Tests shortend

--- Downloader.UnitTest/DownloadViewModelTest.cs
+++ Downloader.UnitTest/DownloadViewModelTest.cs
@@ -18,7 +18,8 @@ namespace Downloader.UnitTest
         //Assert
 
         [Test]
-        public void Given_DownloadUrl_When_DownloadIsAddedToTheOtherDownloads_Then_DownloadIsAppendedToOtherDownloadsWithCorrectPropertyValues()
+        public void
+            Given_DownloadUrl_When_DownloadIsAddedToTheOtherDownloads_Then_DownloadIsAppendedToOtherDownloadsWithCorrectPropertyValues()
         {
             //Arrange    
             MainViewModel mainViewModel = CreateMainViewModel();
@@ -30,7 +31,8 @@ namespace Downloader.UnitTest
             //Assert
             mainViewModel.Downloads.Should().HaveCount(1);
             mainViewModel.Downloads.Single().Name.Should().Be("big_buck_bunny_360p_30mb.mp4");
-            mainViewModel.Downloads.Single().URL.Should().Be("https://sample-videos.com/video123/mp4/360/big_buck_bunny_360p_30mb.mp4");
+            mainViewModel.Downloads.Single().URL.Should()
+                .Be("https://sample-videos.com/video123/mp4/360/big_buck_bunny_360p_30mb.mp4");
             mainViewModel.Downloads.Single().Progress.Should().Be(0.0);
         }
 
@@ -40,15 +42,17 @@ namespace Downloader.UnitTest
             //Arrange
 
             var mockedDownloader = new Mock<IDownloader>();
-            var mockedIEncryptable = CreateMonster();
-            var downloadViewModel = new DownloadViewModel(mockedDownloader.Object, Mock.Of<IMessenger>(), mockedIEncryptable)
-            {
-                URL = "https://sample-videos.com/video123/mkv/360/big_buck_bunny_360p_30mb.mkv",
-                Name = "big_buck_bunny_360p_30mb.mkv"
-            };
+            var mockedIEncryptable = Mock.Of<IEnumerable<IEncryptable>>();
+            var downloadViewModel =
+                new DownloadViewModel(mockedDownloader.Object, Mock.Of<IMessenger>(), mockedIEncryptable)
+                {
+                    URL = "https://sample-videos.com/video123/mkv/360/big_buck_bunny_360p_30mb.mkv",
+                    Name = "big_buck_bunny_360p_30mb.mkv"
+                };
 
             mockedDownloader
-                .Setup(x => x.Start("https://sample-videos.com/video123/mkv/360/big_buck_bunny_360p_30mb.mkv", "big_buck_bunny_360p_30mb.mkv", It.IsAny<IProgress<double>>()))
+                .Setup(x => x.Start("https://sample-videos.com/video123/mkv/360/big_buck_bunny_360p_30mb.mkv",
+                    "big_buck_bunny_360p_30mb.mkv", It.IsAny<IProgress<double>>()))
                 .Callback((string url, string name, IProgress<double> progress) => progress.Report(10));
 
             //Act
@@ -57,14 +61,18 @@ namespace Downloader.UnitTest
             //Assert
             downloadViewModel.Progress.Should().Be(10.0);
 
-            mockedDownloader.Verify(x => x.Start("https://sample-videos.com/video123/mkv/360/big_buck_bunny_360p_30mb.mkv", "big_buck_bunny_360p_30mb.mkv", It.IsAny<IProgress<double>>()), Times.Once());
+            mockedDownloader.Verify(
+                x => x.Start("https://sample-videos.com/video123/mkv/360/big_buck_bunny_360p_30mb.mkv",
+                    "big_buck_bunny_360p_30mb.mkv", It.IsAny<IProgress<double>>()), Times.Once());
         }
 
         [Test]
         public void Given_Downloading_When_DownloadStopButtonIsPressed_Then_DownloadGetsStopped()
         {
             //Arrange
-            var downloadViewModel = CreateIDownloadViewModel(10, "https://sample-videos.com/video123/mkv/360/big_buck_bunny_360p_30mb.mkv", "big_buck_bunny_360p_30mb.mkv");
+            var downloadViewModel = CreateIDownloadViewModel(10,
+                "https://sample-videos.com/video123/mkv/360/big_buck_bunny_360p_30mb.mkv",
+                "big_buck_bunny_360p_30mb.mkv");
             //Act
             downloadViewModel.Object.StopDownload();
 
@@ -74,14 +82,15 @@ namespace Downloader.UnitTest
         }
 
         [Test]
-        [TestCase("sfjsbdf", true)]
+        [TestCase("https://www.cooleseite.de/", true)]
         [TestCase("", false)]
-        public void Given_ButtonIsDeactivated_When_URLTextBoxGetsFilled_Then_ButtonIsActivated(string urlText, bool expectedCanDownload)
+        public void Given_ButtonIsDeactivated_When_URLTextBoxGetsFilled_Then_ButtonIsActivated(string urlText,
+            bool expectedCanDownload)
         {
 
             //Arrange
             var mainViewModel = CreateMainViewModel();
-            
+
             //Act
             mainViewModel.UrlText = urlText;
             //Assert
@@ -103,13 +112,14 @@ namespace Downloader.UnitTest
                     return true;
                 });
 
-            var mockedIEncryptable = CreateMonster();           
+            var mockedIEncryptable = Mock.Of<IEnumerable<IEncryptable>>();
 
-            var downloadViewModel = new DownloadViewModel(mockedDownloader.Object, Mock.Of<IMessenger>(), mockedIEncryptable)
-            {
-                URL = "https://sample-videos.com/video123/mkv/360/big_buck_bunny_360p_30mb.mkv",
-                Name = "big_buck_bunny_360p_30mb.mkv"
-            };
+            var downloadViewModel =
+                new DownloadViewModel(mockedDownloader.Object, Mock.Of<IMessenger>(), mockedIEncryptable)
+                {
+                    URL = "https://sample-videos.com/video123/mkv/360/big_buck_bunny_360p_30mb.mkv",
+                    Name = "big_buck_bunny_360p_30mb.mkv"
+                };
 
             //Act
             downloadViewModel.StartDownload();
@@ -134,19 +144,12 @@ namespace Downloader.UnitTest
                 .Returns(name);
             return mockedDownloadViewModel;
         }
-        
+
         MainViewModel CreateMainViewModel()
         {
             Func<IDownloadViewModel> func = () => Mock.Of<IDownloadViewModel>();
 
             return new MainViewModel(Mock.Of<IMessenger>(), func);
-        }
-
-        IEnumerable<IEncryptable> CreateMonster()
-        {
-            
-
-            return Mock.Of<IEnumerable<IEncryptable>>();
         }
     }
 }
